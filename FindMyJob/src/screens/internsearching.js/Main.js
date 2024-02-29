21125e81a1ee5be2fdcaab02b0cff179337ab318
Karen <karensilinou@gmail.com>
2024-02-29 09:57:04 +0100

    derniere version

--- FindMyJob/src/screens/internsearching.js/Main.js
+++ FindMyJob/src/screens/internsearching.js/Main.js
@@ -1,11 +1,13 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
+const Drawer = createDrawerNavigator();
 const Main = () => {
   return (
-    <View>
-      <Text>Main</Text>
-    </View>
+    <Drawer.Navigator>
+      <Drawer.Screen name="Drawer" component={DrawerScren} />
+    </Drawer.Navigator>
   );
 };
 
