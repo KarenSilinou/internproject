50e1b3d5b9a999ac7fa93a2dd5f31fa44d6c8199
Karen <karensilinou@gmail.com>
2024-03-04 23:36:35 +0100

    derniere version

---
+++ FindMyJob/src/screens/internsearching.js/tabs/Applies.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Applies = () => {
+  return (
+    <View>
+      <Text>Applies</Text>
+    </View>
+  );
+};
+
+export default Applies;
