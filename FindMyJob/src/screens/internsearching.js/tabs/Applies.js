f060d94a6818049482db77e1907799ca02efa14c
Karen <karensilinou@gmail.com>
2024-03-06 19:43:47 +0100

    derniere version

--- FindMyJob/src/screens/internsearching.js/tabs/Applies.js
+++ FindMyJob/src/screens/internsearching.js/tabs/Applies.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useIsFocused} from '@react-navigation/native';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import NoLoginComponent from '../../../common/NoLoginComponent';
 import {BG_COLOR} from '../../../utils/Colors';
 
 const Applies = () => {
+  const isFocused = useIsFocused();
+  const [isLogin, setIsLogin] = useState(false);
+  useEffect(() => {
+    getData();
+  }, [isFocused]);
+  const getData = async () => {
+    const id = await AsyncStorage.getItem('USER_ID');
+    const type = await AsyncStorage.getItem('USER_TYPE');
+    if (id != null && type != null) {
+      if (type == 'user') {
+        setIsLogin(true);
+      }
+    }
+  };
   return (
     <View style={styles.container}>
-      <NoLoginComponent
-        desc={
-          'Pour avoir la trace de vos demandes de stage vous devez creer un compte'
-        }
-        heading={'Une place pour traquer toutes vos demandes'}
-      />
+      {isLogin && (
+        <NoLoginComponent
+          desc={
+            'Pour avoir la trace de vos demandes de stage vous devez creer un compte'
+          }
+          heading={'Une place pour traquer toutes vos demandes'}
+        />
+      )}
     </View>
   );
 };
--- FindMyJob/src/screens/internsearching.js/tabs/Profile.js
+++ FindMyJob/src/screens/internsearching.js/tabs/Profile.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useIsFocused} from '@react-navigation/native';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import NoLoginComponent from '../../../common/NoLoginComponent';
 import {BG_COLOR} from '../../../utils/Colors';
 
 const Profile = () => {
+  const isFocused = useIsFocused();
+  const [isLogin, setIsLogin] = useState(false);
+  useEffect(() => {
+    getData();
+  }, [isFocused]);
+  const getData = async () => {
+    const id = await AsyncStorage.getItem('USER_ID');
+    const type = await AsyncStorage.getItem('USER_TYPE');
+    if (id != null && type != null) {
+      if (type == 'user') {
+        setIsLogin(true);
+      }
+    }
+  };
   return (
     <View style={styles.container}>
-      <NoLoginComponent
-        desc={
-          'Mettez les informations vous concernant pour mieux situer la compagnie'
-        }
-        heading={'Gerez votre profil ici...'}
-      />
+      {isLogin && (
+        <NoLoginComponent
+          desc={
+            'Mettez les informations vous concernant pour mieux situer la compagnie'
+          }
+          heading={'Gerez votre profil ici...'}
+        />
+      )}
     </View>
   );
 };
