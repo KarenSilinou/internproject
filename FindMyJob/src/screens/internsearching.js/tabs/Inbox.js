fcd57e4e9e31059504e601d9135ae40da7cb5c4a
Karen <karensilinou@gmail.com>
2024-03-11 22:52:01 +0100

    derniere version

--- FindMyJob/src/screens/internsearching.js/tabs/Inbox.js
+++ FindMyJob/src/screens/internsearching.js/tabs/Inbox.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useIsFocused} from '@react-navigation/native';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import NoLoginComponent from '../../../common/NoLoginComponent';
 import {BG_COLOR} from '../../../utils/Colors';
 
 const Inbox = () => {
+  const isFocused = useIsFocused();
+  const [isLogin, setIsLogin] = useState(false);
+  useEffect(() => {
+    getData();
+  }, [isFocused]);
+  const getData = async () => {
+    const id = await AsyncStorage.getItem('USER_ID');
+    const type = await AsyncStorage.getItem('USER_TYPE');
+    if (id != null && type != null) {
+      if (type == 'user') {
+        setIsLogin(true);
+      }
+    }
+  };
   return (
     <View style={styles.container}>
-      <NoLoginComponent
-        desc={'Discutez avec des entreprises pour avoir une recommandation'}
-        heading={"Vous pouvez discuter avec l'entreprise"}
-      />
+      {isLogin && (
+        <NoLoginComponent
+          desc={'Discutez avec des entreprises pour avoir une recommandation'}
+          heading={"Vous pouvez discuter avec l'entreprise"}
+        />
+      )}
     </View>
   );
 };
--- FindMyJob/src/screens/internsearching.js/tabs/Profile.js
+++ FindMyJob/src/screens/internsearching.js/tabs/Profile.js
@@ -1,25 +1,54 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import firestore from '@react-native-firebase/firestore';
 import {useIsFocused} from '@react-navigation/native';
 import React, {useEffect, useState} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import NoLoginComponent from '../../../common/NoLoginComponent';
 import {BG_COLOR} from '../../../utils/Colors';
 
 const Profile = () => {
   const isFocused = useIsFocused();
   const [isLogin, setIsLogin] = useState(false);
+  const [userData, setUserData] = useState(null);
+
   useEffect(() => {
     getData();
+    getProfileData();
   }, [isFocused]);
+
   const getData = async () => {
     const id = await AsyncStorage.getItem('USER_ID');
     const type = await AsyncStorage.getItem('USER_TYPE');
+
     if (id != null && type != null) {
       if (type == 'user') {
         setIsLogin(true);
       }
     }
   };
+
+  const getProfileData = async () => {
+    const id = await AsyncStorage.getItem('USER_ID');
+    console.log('ID:', id);
+
+    firestore()
+      .collection('users')
+      .doc(id)
+      .get()
+      .then(data => {
+        console.log('Data from Firestore:', data);
+        if (data.exists) {
+          console.log('Data exists:', data.data());
+          setUserData(data.data());
+        } else {
+          console.log('Document does not exist');
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching document:', error);
+      });
+  };
+
   return (
     <View style={styles.container}>
       {isLogin && (
@@ -27,17 +56,50 @@ const Profile = () => {
           desc={
             'Mettez les informations vous concernant pour mieux situer la compagnie'
           }
-          heading={'Gerez votre profil ici...'}
+          heading={'Gérez votre profil ici...'}
         />
+      )}
+      {!isLogin && (
+        <View style={styles.mainView}>
+          <TouchableOpacity style={{marginLeft: 20, marginTop: 20}}>
+            <Image
+              source={require('../../../images/userPlaceholder.png')}
+              style={styles.profile}
+            />
+          </TouchableOpacity>
+          <Text style={styles.name}>{userData ? userData.name : 'NA'}</Text>
+          <Text style={styles.email}>{userData ? userData.email : 'NA'}</Text>
+        </View>
       )}
     </View>
   );
 };
 
 export default Profile;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: BG_COLOR,
+  },
+  mainView: {
+    flex: 1,
+  },
+  profile: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  name: {
+    fontSize: 30,
+    fontWeight: '600',
+    marginLeft: 20,
+    marginTop: 10,
+  },
+  email: {
+    fontSize: 20,
+    fontWeight: '500',
+    marginLeft: 20,
+    marginTop: 5,
   },
 });
